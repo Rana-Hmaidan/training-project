1a4c9c5d0f4cc6e97b672b169309b0f4fd6f199c
Rana-Hmaidan <32235256+Rana-Hmaidan@users.noreply.github.com>
2018-06-04 18:30:03 +0300

    Add files via upload

--- Parsing2.java
+++ Parsing2.java
@@ -6,9 +6,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Parsing2 { 
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
         String csvFile = "E:\\2009.csv";
         BufferedReader br = null;
@@ -16,52 +21,105 @@ public class Parsing2 {
         String cvsSplitBy = ";";
         
         //create 3 hashtables
-        Hashtable<Integer,String> schools=new Hashtable<Integer,String>();  
-        Hashtable<Integer,String> mohafathat=new Hashtable<Integer,String>(); 
-        Hashtable<Integer,String> masters=new Hashtable<Integer,String>(); 
-   
+        Hashtable<String,Integer> schools=new Hashtable<String,Integer>();  
+        Hashtable<String,Integer> mohafathat=new Hashtable<String,Integer>(); 
+        Hashtable<String,Integer> masters=new Hashtable<String,Integer>(); 
+        
+        //schools.containsKey("العائشية");
+        
+        //اذا كان ال id للمدرسة موجود  ما بضيفه على الداتا بيس اما ازا مش موجود بعطيها id وبضيفها على 
+        //ال id هو ال   hashsize +1
+        //بفحص ازا موجودة بلا hash table 
+        //نفس الاشي للمحافظة وللتخصص 
+        //
         try {
             
              // create BufferedReader for file
             br = new BufferedReader(new FileReader(csvFile));
-            // fill BufferedReader with data from file 
+            
+            // fill BufferedReader with data from file
             while ((line = br.readLine()) != null) {
                 line = new String(line.getBytes("UTF-8"));
                 // create array of string and use comma as separator 
                 String[] student = line.split(cvsSplitBy);
-                   System.out.println("الطالب[المديرية =" +  student[0] + ", المدرسة= " + student[1] + " , رقم الجلوس =" + 
+                 
+                //Year	RegNo	Name	Avg	SchoolId	MohafathaId	MasterId	Is_WestBank
+        
+                int SchoolId=schools.size();
+                int MohafathaId=mohafathat.size();
+                int MasterId=masters.size();
+                
+                int Is_WestBank = 0; //للضفة  0 ولغزة 1
+                
+                
+                try{
+                // Load the JDBC driver
+                 Class.forName("com.mysql.jdbc.Driver");
+                 System.out.println("Driver loaded");
+                  
+                  // Connect to a database
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/tawjihii" , "root", "");
+                System.out.println("Database connected");
+                  
+                 // Create a statement
+                Statement statement = connection.createStatement();
+                
+            //add elements to hash table and increase id for each 
+            //and adding to database table
+               
+               if(!schools.containsKey(student[1])){       
+            //if hash table not contains elemnt adding it 
+                schools.put(student[1],++SchoolId);
+            //insert element to databse              
+                  String sql="INSERT INTO school(school_Id , school_name) VALUES ("+ SchoolId+",'"+ student[1] + "' )";  
+                  statement.executeQuery(sql);
+
+               }
+             //System.out.println(schools);
+             
+                if(!mohafathat.containsKey(student[0])){     
+                mohafathat.put(student[0],++MohafathaId);
+                
+                String sql2="INSERT INTO mohafatha(Mohafatha_Id	, Mohafatha_name) VALUES ("+ MohafathaId+",'"+ student[0]+ "' )";  
+                  statement.executeQuery(sql2);
+                
+   
+                }
+                
+                
+                if(!masters.containsKey(student[9])){   
+                masters.put(student[9],++MasterId);
+                      
+    
+             String sql3="INSERT INTO master(Master_Id	, Master_name) VALUES ("+ MasterId+",'"+ student[9]+ "' )";  
+                  statement.executeQuery(sql3);
+                
+                  }
+                
+               //print elements 
+                 /*  System.out.println("الطالب[المديرية =" +  student[0] + ", المدرسة= " + student[1] + " , رقم الجلوس =" + 
                     student[2]+ ", الاسم الاول =" +  student[3] + ", اسم الاب = " +  student[4] + ", اسم الجد = " + student[5] + 
                     " , اسم العائلة = " +  student[6] +", المجموع = " +  student[7] +", المعدل = " +  student[8] +
                     ", الفرع = " +  student[9] +"]");  
-     
                    
-            for (int i=0;i<student.length;i++){
-               int school_Id =i;
-               String school_name =student[i];
-               
-               if(school_name != ""){
-                    try{
-                //connect to database 
-                  Class.forName("com.mysql.jdbc.Driver");
-
-                  Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/tawjihi" , "root", "");
+                  */
                 
-                  Statement statement = connection.createStatement();
+                int year=2009;
+                String RegNo=student[0];
+                String name=student[3]+student[4]+student[5]+ student[6] ;
+                double avg=Double.parseDouble(student[8]);
+                
+ String sql4="INSERT INTO student(Year,RegNo,Name,Avg ,SchoolId,MohafathaId,MasterId,Is_WestBank) "
+    + "+values ("+  year + ",'"+ RegNo + "','"+ name + "',"+ avg + ","+SchoolId+"," +MohafathaId + ","+MasterId+ ","+Is_WestBank+")"; 
+                             
+             // Close the connection with data base 
+               connection.close(); 
                   
-                  String sql="INSERT INTO school(school_Id , school_name) VALUES ('"+ school_Id+"','"+ school_name + "' )";  
-                  
-                  statement.executeUpdate(sql);
-     
             }catch(Exception e){
-                
+                    System.out.println(" ");
             }
-                    
-               }
             }
-            
-            
-            }
-
+        
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -77,10 +135,8 @@ public class Parsing2 {
         }
         
         
-
         
-    }
-    
-}
+    }//end of main
+}//end of class
 
  
