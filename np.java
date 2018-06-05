48bdac36d62a9e95a962e2771f315ca4c5c42bfe
Rana-Hmaidan <32235256+Rana-Hmaidan@users.noreply.github.com>
2018-06-05 19:08:32 +0300

    Add files via upload

---
+++ np.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.mavenproject2;
+
+import com.blade.Blade;
+import com.blade.mvc.handler.RouteHandler;
+import com.blade.mvc.http.Request;
+import com.blade.mvc.http.Response;
+
+public class np {
+
+    public static void main(String[] args) {
+    /*   
+     Blade.me().get("/search", new RouteHandler(){
+        
+        public void handle(Request request, Response response) {
+                String  search= request.query("s").get();
+                response.json("يوسف");
+            }
+        }).start();
+     
+    */
+       Blade.me().get("/", (req, res) -> {
+        res.text("Hello Blade");
+    }).start();
+    
+      }
+}
